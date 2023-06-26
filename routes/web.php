c1a7f241b91ffc60d4aa7047a730b52a81e88ffa
wahyuex <88044555+wahyuex@users.noreply.github.com>
2023-06-26 13:39:15 +0700

    done semua

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\EmployeeController;
-use App\Http\Controllers\Auth\LoginController;
 use Illuminate\Support\Facades\Auth;
 
 /*
@@ -16,88 +16,16 @@ use Illuminate\Support\Facades\Auth;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-// MOdul 5
-// Route::get('home', [HomeController::class, 'index'])->name('home');
- 
-Route::get('profile', ProfileController::class)->name('profile');
-Route::post('profile', ProfileController::class)->name('profile');
-// Route::put('/nama/{id}', [EmployeeController::class,'update'])->name('employee.update');
-// Route::get('/nama/{id}/edit', [EmployeeController::class,'edit'])->name('employee.edit');
 
-
-Route::resource('employees', EmployeeController::class);
-
-
-// modul 4
 // Route::get('/', function () {
 //     return view('welcome');
 // });
-Route::get('/', function () {
-    return view('auth.login');
-});
-// Route::get('/routing', function() {
-//     return view('routing');
-// });
-// Route::get('/bootstrap', function() {
-//     return view('clonebootstrap');
-// });
 
-// Route::get('/basic_routing', function() {
-//     return 'Hello World';
-// });
-
-// Route::view('/view_route', 'view_route');
-// Route::view('/view_route', 'view_route', ['name' => 'Wahyu']);
-
-
-// Route::get('/controller_route', [RouteController::class, 'index']);
-
-// // Route::redirect('/', '/routing');
-
-// Route::get('/user/{id}', function($id) {
-//     return "User Id: ".$id;
-// });
-// Route::get('/posts/{post}/comments/{comment}', function($postId, $commentId) {
-//     return "Post Id: ".$postId.", Comment Id: ".$commentId;
-// });
-
-// Route::get('username/{name?}', function($name = null) {
-//     return 'Username: '.$name;
-// });
-
-// Route::get('/title/{title}', function($title) {
-//     return "Title: ".$title;
-// })->where('title', '[A-Za-z]+');
-
-// Route::get('/profile/{profileId}', [RouteController::class, 'profile'])->name('profileRouteName');
-
-// Route::get('/route_priority/{rpId}', function($rpId) {
-//     return "This is Route One";
-// });
-// Route::get('/route_priority/user', function() {
-//     return "This is Route 1";
-// });
-// Route::get('/route_priority/user', function() {
-//     return "This is Route 2";
-// });
-// Route::fallback(function() {
-//     return 'This is Fallback Route';
-// });
-
-// Route::prefix('admin')->name('admin.')->group(function() {
-//     Route::get('/dashboard', function() {
-//         return "This is admin dashboard";
-//     })->name('dashboard');
-//     Route::get('/users', function() {
-//         return "This is user data on admin page";
-//     })->name('users');
-//     Route::get('/items', function() {
-//         return "This is item data on admin page";
-//     })->name('items');
-// });
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-Route::post('/login', [LoginController::class, 'authenticate']);
+Route::group(['middleware'=>'auth'],function(){
+    Route::get('/', [HomeController::class, 'index'])->name('home');
+    Route::post('profile', ProfileController::class)->name('profile');
+    Route::resource('employees', EmployeeController::class);
+});
